8a321e9b869b251b7c576ea60c24ae550db308a5
Luke Bjerring <lukebjerring@google.com>
2018-08-29 09:54:59 -0400

    Add util for fetching an interop run

---
+++ metrics/fetch_interop.go
@@ -0,0 +1,14 @@
+package metrics
+
+import "github.com/web-platform-tests/wpt.fyi/shared"
+
+// FetchInterop fetches the PassRateMetadata for the given sha / labels, using
+// the API on the given host.
+func FetchInterop(wptdHost string, filter shared.TestRunFilter) PassRateMetadata {
+	url := "https://" + wptdHost + "/api/interop"
+	url += "?" + filter.ToQuery(true).Encode()
+
+	var interop PassRateMetadata
+	shared.FetchJSON(url, &interop)
+	return interop
+}
