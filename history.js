acfd2c1b9749a2439b3187ab69c34f0e1397832c
Philip Jägenstedt <philip@foolip.org>
2018-11-08 13:32:11 +0100

    Make history.js output CSV

--- history.js
+++ history.js
@@ -1,69 +1,87 @@
+'use strict';
+
 const fetch = require ('node-fetch');
+const fs = require('fs');
+const metrics = require('./metrics.js');
 
 const PRODUCTS = ['chrome', 'edge', 'firefox', 'safari'];
 
-// doens't work because of https://github.com/web-platform-tests/wpt.fyi/issues/733
-/*
-async function findProductRuns(product) {
-    const allRuns = [];
-    // paginate using the `to` parameter, starting with the current time and
-    // using the date of the last run from each response until all are found.
-    let toDate = new Date().toISOString();
-    while (true) {
-        const url = `https://wpt.fyi/api/runs?product=${product}&label=stable&to=${toDate}&max-count=500`;
-        let runs = await (await fetch(url)).json();
-        if (runs.length === 0) {
-            // no more runs
-            break;
-        }
-        allRuns.push(...runs);
-        toDate = runs[runs.length - 1].time_start;
-    }
-    return allRuns;
-}
-*/
+const RESULTS_URL_PREFIX = 'https://storage.googleapis.com/wptd-results/'
+const LOCAL_RESULTS_PATH = '../wptd-results/'
 
-async function getAllRuns() {
-    const allRuns = [];
-    // Paginate using the `to` parameter, starting with the current time and
-    // using the date of the last run from each response until all are found.
-    // The use of `fromDate` is to avoid
-    // https://github.com/web-platform-tests/wpt.fyi/issues/733 and is OK
-    // because that's when Edge and Safari runs began.
-    const fromDate = '2017-08-01T00:00:00Z';
-    let toDate = new Date().toISOString();
-    while (true) {
-        const url = `https://wpt.fyi/api/runs?labels=stable&from=${fromDate}&to=${toDate}&max-count=500`;
-        let runs = await (await fetch(url)).json();
-        if (runs.length === 0) {
-            // no more runs
-            break;
-        }
-        allRuns.push(...runs);
-        toDate = runs[runs.length - 1].time_start;
-    }
-    return allRuns;
+const SCORING_OPTIONS = {
+    normalizePerTest: true,
+    requireHarnessOK: true,
 }
 
-async function findAlignedRuns() {
-    const productRuns = await Promise.all(PRODUCTS.map(async product => {
-        return product;
-    }));
-    let toDate = new Date().toISOString();
-    const runsUrl = `https://wpt.fyi/api/runs?labels=stable&products=${PRODUCTS.join(',')}&aligned&max-count=50`;
-    const runs = await (await fetch(runsUrl)).json();
+async function getStableRuns(sha) {
+    const url = `https://wpt.fyi/api/runs?labels=stable&sha=${sha}&max-count=500`;
+    let runs = await (await fetch(url)).json();
     return runs;
 }
 
-async function main() {
-    const runs = await getAllRuns();
-    for (const product of PRODUCTS) {
-        for (const run of runs) {
-            if (run.browser_name !== product) {
-                continue;
-            }
-            console.log(run.browser_name, run.browser_version, run.time_start);
+function pickBestRun(product, runs) {
+    const candidates = runs.filter(r => r.browser_name === product);
+    /*
+    if (candidates.length !== 1) {
+        console.log(`${product} has ${runs.length} candidate runs:`);
+        for (const run of candidates) {
+            console.log(`  ${run.raw_results_url}`);
         }
+    }
+    */
+    return candidates[0];
+}
+
+async function main() {
+    const csvHeader = ['date', 'sha', ...PRODUCTS, 'total'];
+    console.log(csvHeader.join(','));
+
+    const maybeAlignedRuns = fs.readFileSync('aligned-shas.txt', 'UTF-8')
+        .split('\n').filter(l => l).map(l => l.split(' '));
+
+    let skipCounter = 0;
+
+    for (const [date, sha] of maybeAlignedRuns) {
+        const runs = await getStableRuns(sha);
+
+        const bestRuns = PRODUCTS.map(p => pickBestRun(p, runs));
+        if (bestRuns.some(run => !run)) {
+            //console.warn(date, sha, 'MISSING SOME RUNS');
+            continue;
+        }
+
+        if (skipCounter++ % 10 !== 0) {
+            //continue;
+        }
+
+        //console.log(date, sha, 'OK');
+        const scores = {};
+        for (const run of bestRuns) {
+            const product = run.browser_name;
+            //if (product !== 'chrome') continue;
+            //console.log(date, sha, product);
+
+            const resultsUrl = run.raw_results_url;
+            if (!resultsUrl.startsWith(RESULTS_URL_PREFIX)) {
+                throw new Error(`Unexpected results URL: ${resultsUrl}`);
+            }
+
+            const resultsPath = LOCAL_RESULTS_PATH + resultsUrl.substr(RESULTS_URL_PREFIX.length);
+            if (!fs.existsSync(resultsPath)) {
+                throw new Error(`Local copy of results not found: ${resultsPath}`);
+            }
+
+            const report = JSON.parse(fs.readFileSync(resultsPath, 'UTF-8'));
+            const [score, total] = metrics.scoreReport(report, SCORING_OPTIONS);
+            scores[product] = { score, total };
+        }
+
+        const productScores = PRODUCTS.map(p => Math.floor(scores[p].score));
+        const productTotals = PRODUCTS.map(p => scores[p].total);
+        const maxTotal = productTotals.reduce((x, y) => Math.max(x, y));
+        const csvRecord = [date.substr(0, 10), sha.substr(0, 10), ...productScores, maxTotal];
+        console.log(csvRecord.join(','));
     }
 }
 
