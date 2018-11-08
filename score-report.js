bfcbb6d05411a7b1167d6f41c3acb09d135fbaa9
Philip Jägenstedt <philip@foolip.org>
2018-11-08 14:25:13 +0100

    Allow score-report.js to score a local report

--- score-report.js
+++ score-report.js
@@ -1,4 +1,5 @@
 const fetch = require ('node-fetch');
+const fs = require('fs');
 const metrics = require('./metrics.js');
 
 async function main() {
@@ -7,7 +8,25 @@ async function main() {
     requireHarnessOK: true,
   }
 
-  const productSpec = process.argv[2];
+  const pathOrProductSpec = process.argv[2];
+  let report;
+  if (fs.existsSync(pathOrProductSpec)) {
+    const path = pathOrProductSpec;
+    report = JSON.parse(fs.readFileSync(path, 'UTF-8'));
+  } else {
+    const productSpec = pathOrProductSpec;
+    const runsUrl = `https://wpt.fyi/api/runs?product=${productSpec}`
+    const runsInfo = await (await fetch(runsUrl)).json();
+    if (runsInfo.length === 0) {
+      console.error(`no run found for ${productSpec}`);
+      process.exit(1);
+    }
+    const info = runsInfo[0];
+    const resultsUrl = info.raw_results_url;
+
+    report = await (await fetch(resultsUrl)).json();
+  }
+
   let testPrefix = process.argv[3];
   if (testPrefix) {
     if (!testPrefix.startsWith('/')) {
@@ -16,16 +35,6 @@ async function main() {
     options.testFilter = test => test.test.startsWith(testPrefix);
   }
 
-  const runsUrl = `https://wpt.fyi/api/runs?product=${productSpec}`
-  const runsInfo = await (await fetch(runsUrl)).json();
-  if (runsInfo.length === 0) {
-    console.error(`no run found for ${productSpec}`);
-    process.exit(1);
-  }
-  const info = runsInfo[0];
-  const resultsUrl = info.raw_results_url;
-
-  const report = await (await fetch(resultsUrl)).json();
   // sort to make output nicer, doesn't affect scoring
   report.results.sort((a, b) => {
     return a.test.localeCompare(b.test);
