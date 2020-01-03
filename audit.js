9b5f9b351f10bb04e6b90a98ad98140549751502
Philip Jägenstedt <philip@foolip.org>
2020-01-03 16:32:04 +0100

    Iterate runs in audit.js instead of getting all up front (#20)

--- audit.js
+++ audit.js
@@ -1,11 +1,11 @@
+// For https://github.com/web-platform-tests/wpt.fyi/issues/1175
+
 'use strict';
 
 const lib = require('./lib');
 
 async function main() {
-  const runs = await lib.runs.getAll();
-
-  for (const run of runs) {
+  for await (const run of lib.runs.getIterator()) {
     const labels = new Set(run.labels);
     if (!labels.has('master') && !labels.has('pr_base') && !labels.has('pr_head')) {
       console.log(JSON.stringify(run));
